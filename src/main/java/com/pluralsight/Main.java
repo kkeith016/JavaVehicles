53de3379b0301c9e9d4826705f34e8b8db59d1ef
Kylee <kkeith.biz@gmail.com>
2025-10-27 11:42:19 -0400

    Printing instance made

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -16,6 +16,23 @@ public class Main {
         familyCar.setNumberOfPassangers(8);
         familyCar.setCargoCapacity(500);
 
+        SemiTruck bigRig = new SemiTruck();
+        bigRig.setColor("Red");
+        bigRig.setFuelCapacity(1000);
+        bigRig.setNumberOfPassangers(4);
+        bigRig.setCargoCapacity(10000);
 
+        Hovercraft airSkimmer = new Hovercraft();
+        airSkimmer.setColor("Military Green");
+        airSkimmer.setFuelCapacity(10000);
+        airSkimmer.setNumberOfPassangers(400);
+        airSkimmer.setCargoCapacity(100000);
+
+        //Printing
+
+        System.out.println("Moped: " + slowRide);
+        System.out.println("Car: " + familyCar);
+        System.out.println("SemiTruck: " + bigRig);
+        System.out.println("Hovercraft: " + airSkimmer);
     }
 }
